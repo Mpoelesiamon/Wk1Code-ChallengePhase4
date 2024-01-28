23a4ef0b117c8544bfc602ce85cfdf8b7e30bb1c
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-01-28 18:42:24 +0300

    app and seed

--- python-code-challenge-superheroes/code-challenge/app/app.py
+++ python-code-challenge-superheroes/code-challenge/app/app.py
@@ -1,22 +1,120 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, request, jsonify
 from flask_migrate import Migrate
+from flask_restful import Api,Resource
+from werkzeug.exceptions import NotFound
+# from sqlalchemy.ext.declarative import declarative_base
 
-from models import db, Hero
+from models import db, Hero, Power, HeroPower
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# base = declarative_base()
+
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
+api= Api(app)
+
+@app.errorhandler(NotFound)
+def handle_not_found(e):
+    response= make_response("NotFound: The requested resource not found", 404)
+    return response
+
 @app.route('/')
 def home():
-    return ''
+    return "Week 1 code challenge"
 
+class Heroes(Resource):
+    def get(self):
+        response_dict= [n.to_dict() for n in Hero.query.all()]
+        response= make_response(response_dict, 200)
+        return  response
 
+api.add_resource(Heroes, '/heroes')
+
+class HeroesByID(Resource):
+    def get(self,id):
+        record=  Hero.query.filter_by(id=id).first()
+        record_dict=  record.to_dict() if record else None
+        if record is None:
+            error_dict=  {"error": "Hero not found"}
+            response= make_response(error_dict, 404 )
+            return response
+        else:
+            response= make_response(record_dict, 200)
+            return response
+        
+api.add_resource(HeroesByID, '/heroes/<int:id>')
+
+class Powers(Resource):
+    def get(self):
+        response_dict= [n.to_dict() for n in Power.query.all()]
+        
+        response= make_response(response_dict, 200)
+        return  response
+
+api.add_resource(Powers, '/powers')
+
+class PowersByID(Resource):
+    def get(self,id):
+        record=  Power.query.filter_by(id=id).first()
+        record_dict= record.to_dict() if record else None
+        
+        if record_dict== None:
+            error_dict=  {'error': 'Power not found'}
+            response= make_response(error_dict, 404)
+            return response
+        else:
+            response= make_response(record_dict, 200)
+            return response
+    
+    def patch(self,id):
+        power = Power.query.filter_by(id=id).first()
+        record_dict= power.to_dict() if power else None
+        
+        if record_dict== None:
+            error_dict=  {'error': 'Power not found'}
+            response= make_response(error_dict, 404)
+            return response
+        else:
+            for attr in request.form:
+                setattr(power, attr, request.form[attr])
+            
+            db.session.add(power)
+            try:
+                db.session.commit()
+                power_dict= power.to_dict()
+                response= make_response(power_dict, 200)
+                return response
+            except:
+                err_dict= {"errors" : ["validation errors"]}
+                response= make_response(err_dict, 404)
+                db.session.rollback()
+                return response         
+
+api.add_resource(PowersByID, '/powers/<int:id>')
+
+class HeroPowers(Resource):
+    def post(self):
+        new_record=  HeroPower(
+            strength= request.form['strength'],
+            power_id=  int(request.form['power_id']),
+            hero_id=   int(request.form['hero_id']),
+        )
+        db.session.add(new_record)
+        db.session.commit()
+        
+        response_dict= new_record.to_dict()
+        
+        response= make_response(jsonify(response_dict), 201)
+        return response
+    
+api.add_resource(HeroPowers, '/hero_powers')   
+     
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
---
+++ python-code-challenge-superheroes/code-challenge/app/seed.py
@@ -0,0 +1,78 @@
+from models import Hero, HeroPower, Power, db
+from app  import app
+from random import choice as rc
+
+with app.app_context():
+  Hero.query.delete()
+  HeroPower.query.delete()
+  Power.query.delete()
+  
+  
+
+
+#puts "🦸‍♀️ Seeding powers..."
+  powers = [
+  { 'name': "super strength", 'description': "gives the wielder super-human strengths" },
+    { 'name': "flight", 'description': "gives the wielder the ability to fly through the skies at supersonic speed" },
+    { 'name': "super human senses", 'description': "allows the wielder to use her senses at a super-human level" },
+    { 'name': "elasticity", 'description': "can stretch the human body to extreme lengths" }
+  ]
+  
+  for data in powers:
+        power = Power(**data)
+        db.session.add(power)
+
+        db.session.commit()
+
+# #puts "🦸‍♀️ Seeding heroes..."
+  heroes = [
+    { 'name': "Kamala Khan", 'super_name': "Ms. Marvel" },
+    { 'name': "Doreen Green", 'super_name': "Squirrel Girl" },
+    { 'name': "Gwen Stacy", 'super_name': "Spider-Gwen" },
+    { 'name': "Janet Van Dyne", 'super_name': "The Wasp" },
+    { 'name': "Wanda Maximoff", 'super_name': "Scarlet Witch" },
+    { 'name': "Carol Danvers", 'super_name': "Captain Marvel" },
+    { 'name': "Jean Grey", 'super_name': "Dark Phoenix" },
+    { 'name': "Ororo Munroe", 'super_name': "Storm" },
+    { 'name': "Kitty Pryde", 'super_name': "Shadowcat" },
+    { 'name': "Elektra Natchios", 'super_name': "Elektra" }
+  ]
+
+  for data in heroes:
+          #The (**) syntax is used to unpack a dictionary and pass it as keyword arguments to a function or class constructor. 
+          #In this case, thedatadictionary contains the key-value pairs that will be used to initialize theHero` instance.
+          hero = Hero(**data)
+          db.session.add(hero)
+
+          db.session.commit()
+
+# #puts "🦸‍♀️ Adding powers to heroes..."
+
+  strengths = ["Strong", "Weak", "Average"]
+  
+  all_heroes_power = Hero.query.all()
+  all_power = Power.query.all()
+  
+  hero_power = []
+  
+  for n in range (4):
+    for hero in all_heroes_power :
+      power= rc(all_power)
+      heroes_power= rc(all_heroes_power)
+      power_item = HeroPower(strength = rc(strengths),  hero_id=heroes_power.id, power_id=power.id)
+      hero_power.append(power_item)
+
+    db.session.add_all(hero_power)
+    db.session.commit()
+    
+  
+  # Hero.all.each do |hero|
+  #   rand(1..3).times do
+  #     # get a random power
+  #     power = Power.find(Power.pluck(:id).sample)
+
+  #     HeroPower.create!(hero_id: hero.id, power_id: power.id, strength: strengths.sample)
+  #   end
+  # end 
+
+# #puts "🦸‍♀️ Done seeding!"
